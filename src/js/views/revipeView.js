7d185669c94d51bb97fbec2fbfee4750e4082de7
Fani2000 <42602674+Fani2000@users.noreply.github.com>
2022-08-05 18:19:48 +0200

    Added the Fractional package to format the quantity number into fractions

--- src/js/views/revipeView.js
+++ src/js/views/revipeView.js
@@ -1,5 +1,9 @@
 import icons from 'url:../../img/icons.svg';
 
+import { Fraction } from 'fractional';
+
+// console.log(new Fraction(0.5).toString());
+
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
   #data;
@@ -84,22 +88,7 @@ class RecipeView {
 <div class="recipe__ingredients">
   <h2 class="heading--2">Recipe ingredients</h2>
   <ul class="recipe__ingredient-list">
-    ${this.#data.ingredients
-      .map(r => {
-        return `
-    <li class="recipe__ingredient">
-      <svg class="recipe__icon">
-        <use href="${icons}#icon-check"></use>
-      </svg>
-      <div class="recipe__quantity">${r.quantity}</div>
-      <div class="recipe__description">
-        <span class="recipe__unit">${r.unit}</span>
-        ${r.description}
-      </div>
-    </li>
-      `;
-      })
-      .join('')}
+    ${this.#data.ingredients.map(this.#generateIngredientMarkup).join('')}
 
 
   </ul>
@@ -125,6 +114,22 @@ class RecipeView {
     </svg>
   </a>
 </div>
+    `;
+  }
+  #generateIngredientMarkup(ingredient) {
+    return `
+    <li class="recipe__ingredient">
+      <svg class="recipe__icon">
+        <use href="${icons}#icon-check"></use>
+      </svg>
+      <div class="recipe__quantity">${
+        ingredient.quantity ? new Fraction(ingredient.quantity).toString() : ''
+      }</div>
+      <div class="recipe__description">
+        <span class="recipe__unit">${ingredient.unit}</span>
+        ${ingredient.description}
+      </div>
+    </li>
     `;
   }
 }
